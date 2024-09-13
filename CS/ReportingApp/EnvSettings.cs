dbdb27346b9f14adc6bce5c8f73c930adbabca18
Natalia Kazakova <17986517+natakazakova@users.noreply.github.com>
2024-09-13 14:42:48 +0400

    Update EnvSettings.cs

--- CS/ReportingApp/EnvSettings.cs
+++ CS/ReportingApp/EnvSettings.cs
@@ -4,6 +4,6 @@ namespace ReportingApp {
     public static class EnvSettings {
         public static string AzureOpenAIEndpoint { get { return Environment.GetEnvironmentVariable("OPENAI_ENDPOINT"); } }
         public static string AzureOpenAIKey { get { return Environment.GetEnvironmentVariable("OPENAI_APIKEY"); } }
-        public static string DeploymentName { get { return "GPT4o"; } } //Gpt35Turbo, gpt35turbo16k, GPT4, GPT4o
+        public static string DeploymentName { get { return "GPT4o"; } }
     }
 }
