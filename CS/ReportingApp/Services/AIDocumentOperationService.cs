074bb4c08bf15dc3a8db0226f3f9534368081f21
Marcin Rabiak <marcin.rabiak@devexpress.com>
2025-08-11 12:39:03 +0200

    rename assistantId to assistantName

--- CS/ReportingApp/Services/AIDocumentOperationService.cs
+++ CS/ReportingApp/Services/AIDocumentOperationService.cs
@@ -18,10 +18,10 @@ namespace ReportingApp.Services {
         public override async Task<DocumentOperationResponse> PerformOperationAsync(DocumentOperationRequest request, PrintingSystemBase printingSystem, PrintingSystemBase printingSystemWithEditingFields) {
             using(var stream = new MemoryStream()) {
                 printingSystem.ExportToPdf(stream, printingSystem.ExportOptions.Pdf);
-                var assistantId = await AIAssistantProvider.CreateDocumentAssistant(stream);
+                var assistantName = await AIAssistantProvider.CreateDocumentAssistant(stream);
                 return new DocumentOperationResponse {
                     DocumentId = request.DocumentId,
-                    CustomData = assistantId,
+                    CustomData = assistantName,
                     Succeeded = true
                 };
             }
